081cb7b3dfbab17d9c5d793a1a3c02c3fdcf6acf
Marco Frank <47047767+MrPoloGit@users.noreply.github.com>
2026-03-01 22:25:29 +0000

    fixed rtl.f

--- src/rtl/rtl.f
+++ src/rtl/rtl.f
@@ -4,30 +4,24 @@ uart_rx.sv
 uart_tx.sv
 
 # Voxel Bin Architecture
-src/rtl/voxel_bin_architecture/dvs_gesture_accel.sv
-src/rtl/voxel_bin_architecture/GestureClassifier.sv
-src/rtl/voxel_bin_architecture/InputFIFO.sv
-src/rtl/voxel_bin_architecture/MotionComputer.sv
-src/rtl/voxel_bin_architecture/OutputRegister.sv
-src/rtl/voxel_bin_architecture/SpatialCompressor.sv
-src/rtl/voxel_bin_architecture/SystolicMatrixMultiply.sv
-src/rtl/voxel_bin_architecture/TemporalAccumulator.sv
-src/rtl/voxel_bin_architecture/TimeSurfaceBinning.sv
-src/rtl/voxel_bin_architecture/voxel_bin_processed_top.sv
-src/rtl/voxel_bin_architecture/voxel_bin_raw_top.sv
-src/rtl/voxel_bin_architecture/WeightROM.sv
+src/rtl/voxel_bin_architecture/evt2_decoder.sv
+src/rtl/voxel_bin_architecture/gesture_classifier.sv
+src/rtl/voxel_bin_architecture/input_fifo.sv
+src/rtl/voxel_bin_architecture/systolic_array.sv
+src/rtl/voxel_bin_architecture/voxel_bin_core.sv
+src/rtl/voxel_bin_architecture/voxel_bin_top.sv
+src/rtl/voxel_bin_architecture/voxel_binning.sv
+src/rtl/voxel_bin_architecture/weight_ram.sv
 
 # Gradient Map Architecture
 src/rtl/gradient_map_architecture/evt2_decoder.sv
-src/rtl/gradient_map_architecture/flatten_buffer.sv
-src/rtl/gradient_map_architecture/gradient_map_processed_top.sv
-src/rtl/gradient_map_architecture/gradient_map_raw_top.sv
+src/rtl/gradient_map_architecture/gesture_classifier.sv
+src/rtl/gradient_map_architecture/gradient_map_core.sv
+src/rtl/gradient_map_architecture/gradient_map_top.sv
+src/rtl/gradient_map_architecture/gradient_mapping.sv
 src/rtl/gradient_map_architecture/input_fifo.sv
-src/rtl/gradient_map_architecture/spatio_temporal_classifier.sv
 src/rtl/gradient_map_architecture/systolic_array.sv
-src/rtl/gradient_map_architecture/time_surface_encoder.sv
-src/rtl/gradient_map_architecture/time_surface_memory.sv
-src/rtl/gradient_map_architecture/weight_rom.sv
+src/rtl/gradient_map_architecture/weight_ram.sv
 
 # Include chisel files
 -f rtl/chisel/filelist.f
